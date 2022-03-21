7e9dc919cc79ecb81fa4a626adfcef1e49e25d0f
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-03-21 14:44:06 +0100

    Home, About and Article Details pages finished

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  imgPath : String = 'https://images.unsplash.com/photo-1559608622-d443df453289?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-  imgSource : String = 'https://unsplash.com/s/photos/self-care'
+  imgPath : String = 'https://images.unsplash.com/photo-1507652955-f3dcef5a3be5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  // imgSource : String = 'https://unsplash.com/s/photos/self-care'
   constructor() { }
 
   ngOnInit(): void {
